1824e846d31795b3b4042314544940a9f2389b5d
bd21 <bd21@uw.edu>
2021-04-03 12:35:50 -0700

    graph project skeleton:

---
+++ offline/graph-project/data_sources.py
@@ -0,0 +1,28 @@
+
+'''
+Pull data sources from the terra ecosystem, specifically
+ - Prices
+ - Exchange Rates
+to extract insights such as
+ - asset flow
+ - relative and absolute changes (tied to the dollar)
+ - best LP farming methods
+'''
+
+def main():
+    pass
+
+def get_terra_prices():
+    pass
+
+def get_mirror_prices():
+    pass
+
+def get_mirror_prices():
+    pass
+
+def get_anc_prices():
+    pass
+
+def get_terraswap_rates():
+    pass
